55a1e6b5fd24db664f0d3916f9dc2ae937130612
iyar-avital <57311417+iyar-avital@users.noreply.github.com>
2020-05-03 12:34:54 +0300

    03.05 - add Triangle Test

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -7,6 +7,8 @@ import primitives.Vector;
 
 import java.util.List;
 
+import static primitives.Util.isZero;
+
 /**
  * triangle class
  */
@@ -24,40 +26,24 @@ public class Triangle extends Polygon {
 
     @Override
     public List<Point3D> findIntersections(Ray ray) {
+        List<Point3D> intersections = _plane.findIntersections(ray);
+        if (intersections == null) return null;
 
-        //if the ray point begin on a vertex in the triangle
-        if(this._vertices.get(0).equals(ray.get_poo()) || this._vertices.get(1).equals(ray.get_poo()) || this._vertices.get(2).equals(ray.get_poo()))
-            return null;
+        Point3D p0 = ray.get_poo();
+        Vector v = ray.get_direction();
 
-        try {
-            Vector v1 = this._vertices.get(0).subtract(ray.get_poo());
-            Vector v2 = this._vertices.get(1).subtract(ray.get_poo());
-            Vector v3 = this._vertices.get(2).subtract(ray.get_poo());
-            System.out.print(v1 + "-v1       \n" + v2 + "-v2      \n" + v3 + "-v3      \n");
+        Vector v1 = _vertices.get(0).subtract(p0);
+        Vector v2 = _vertices.get(1).subtract(p0);
+        Vector v3 = _vertices.get(2).subtract(p0);
 
-            if(v1.isParallel(v2) || v1.isParallel(v3) || v2.isParallel(v3))
-                return null;
-            Vector N1 = v1.crossProduct(v2);
-            Vector N2 = v2.crossProduct(v3);
-            Vector N3 = v1.crossProduct(v3);
-            System.out.print(N1 + "-N1       \n" + N2 + "-N2      \n" + N3 + "-N3      \n");
+        double s1 = v.dotProduct(v1.crossProduct(v2));
+        if (isZero(s1)) return null;
+        double s2 = v.dotProduct(v2.crossProduct(v3));
+        if (isZero(s2)) return null;
+        double s3 = v.dotProduct(v3.crossProduct(v1));
+        if (isZero(s3)) return null;
 
+        return ((s1 > 0 && s2 > 0 && s3 > 0) || (s1 < 0 && s2 < 0 && s3 < 0)) ? intersections : null;
 
-        double n1 = ray.get_direction().dotProduct(N1);
-        double n2 = ray.get_direction().dotProduct(N2);
-        double n3 = ray.get_direction().dotProduct(N3);
-        System.out.print(ray.get_direction() + "\n");
-            System.out.print(n1 + "-n1       \n" + n2 + "-n2      \n" + n3 + "-n3      \n");
-        if (Util.isZero(n1) || Util.isZero(n2) || Util.isZero(n3))
-            return null;
-
-        if (n1 > 0 && n2 > 0 && n3 > 0)
-            return this._plane.findIntersections(ray);
-
-        if (n1 < 0 && n2 < 0 && n3 < 0)
-            return this._plane.findIntersections(ray);
-        return null;
-
-        } catch (IllegalArgumentException i) { return null; }
     }
 }
--- src/unittests/TriangleTest.java
+++ src/unittests/TriangleTest.java
@@ -6,61 +6,42 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class TriangleTest extends Object {
 
     @Test
     public void findIntersectionsTest() {
-        Triangle triangle = new Triangle(new Point3D(1,0,2),new Point3D(0,0,2),new Point3D(1,1,1));
         // ============ Equivalence Partitions Tests ==============
-        //Inside triangle
-        Ray ray = new Ray(new Point3D(0,1,-2), new Vector(0.5,0,0.86876));
-        assertEquals("Inside triangle", triangle.findIntersections(ray).size(), 1);
 
-        /**Ray ray = new Ray(new Point3D(0,0,0), new Vector(5,3,25));
-        assertEquals("Inside triangle", triangle.findIntersections(ray).size(), 1);*/
-
-        //Outside against edge
-        Ray ray1 = new Ray(new Point3D(1,1,0), new Vector(3,1,5));
-        assertNull("Outside against edge", triangle.findIntersections(ray1));
-
-        /**Ray ray2 = new Ray(new Point3D(0,0,0), new Vector(3,7,5));
-        assertNotNull("Outside against edge", triangle.findIntersections(ray2));*/
-
-        //Outside against vertex
-        Ray ray3 = new Ray(new Point3D(1,0,0), new Vector(1,5,4));
-        assertNull("Outside against vertex", triangle.findIntersections(ray3));
-
-        /**Ray ray3 = new Ray(new Point3D(0,0,0), new Vector(9,11,5));
-        assertNotNull("Outside against vertex", triangle.findIntersections(ray3));*/
+        // TC01: Ray's line is Inside triangle (1 points)
+        Triangle triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        Ray ray = new Ray(new Point3D(0,0.5,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is Inside triangle", List.of(new Point3D(0,0.5,0)) ,triangle.findIntersections(ray));
+        // TC01: Ray's line is Outside against edge (0 points)
+        triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        ray = new Ray(new Point3D(2,0.5,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is Outside against edge", null ,triangle.findIntersections(ray));
+        // TC01:  Ray's line is Outside against vertex (0 points)
+        triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        ray = new Ray(new Point3D(0,2,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is Outside against vertex", null ,triangle.findIntersections(ray));
 
         // =============== Boundary Values Tests ==================
-        //On edge
-        Ray ray4 = new Ray(new Point3D(0.5,0,-3), new Vector(2,1,11));
-        assertNull("On edge", triangle.findIntersections(ray4));
 
-        /**Ray ray4 = new Ray(new Point3D(0,0,0), new Vector(1,1,2));
-        assertNotNull("On edge", triangle.findIntersections(ray4));
-        Ray ray5 = new Ray(new Point3D(0,0,0), new Vector(7,4,10));
-        assertNotNull("On edge", triangle.findIntersections(ray5));*/
-
-        //In vertex
-        Ray ray6 = new Ray(new Point3D(1,0,0), new Vector(1,5,5));
-        assertNull("In vertex", triangle.findIntersections(ray6));
-
-        Ray ray7 = new Ray(new Point3D(1,0,0.5), new Vector(1,7,4));
-        assertNull("In vertex", triangle.findIntersections(ray7));
-
-        /**Ray ray6 = new Ray(new Point3D(0,0,0), new Vector(0,0,5));
-        assertNotNull("In vertex", triangle.findIntersections(ray6));*/
-
-        //On edge's continuation
-        Ray ray8 = new Ray(new Point3D(-0.5,0,-3), new Vector(1.9,-2,7));
-        assertNull("On edge's continuation", triangle.findIntersections(ray8));
-
-        /**Ray ray8 = new Ray(new Point3D(0,0,0), new Vector(3,-4,10));
-        assertNotNull("In vertex", triangle.findIntersections(ray8 ));*/
+        // TC01: Ray's line is On edge (0 points)
+        triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        ray = new Ray(new Point3D(0.5,0,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is On edge", null ,triangle.findIntersections(ray));
+        // TC01: Ray's line is In vertex (0 points)
+        triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        ray = new Ray(new Point3D(0,1,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is In vertex", null ,triangle.findIntersections(ray));
+        // TC01: Ray's line is On edge's continuation (0 points)
+        triangle = new Triangle(new Point3D(-1,0,0), new Point3D(1,0,0) , new Point3D(0,1,0));
+        ray = new Ray(new Point3D(2,0,-1), new Vector(0,0,1));
+        assertEquals("Bad intersects to triangle - line is On edge's continuation", null ,triangle.findIntersections(ray));
     }
-
 }

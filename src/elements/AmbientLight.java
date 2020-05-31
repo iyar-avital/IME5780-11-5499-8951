f57147096557988bbed0eb1752a132e0f1be6e1d
iyar-avital <57311417+iyar-avital@users.noreply.github.com>
2020-05-31 14:52:52 +0300

    Update AmbientLight.java

--- src/elements/AmbientLight.java
+++ src/elements/AmbientLight.java
@@ -4,19 +4,12 @@ import primitives.Color;
 
 public class AmbientLight {
     Color _intensity;
-    
-/**
-constructor
-receiving Color i and double k
-*/
+
     public AmbientLight(Color i, double k)
     {
         _intensity = i.scale(k);
     }
-    
-/**
-function thaet return intensity
-*/
+
     public Color get_intensity() {
         return _intensity;
     }
